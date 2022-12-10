4018dea753a6b7314c0c0028361aa3693f5dbf14
oddy-bassey <oddybassey@gmail.com>
2022-12-10 07:06:42 +0100

    creating threads in Java

---
+++ mutithreading/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("We are now in thread "+Thread.currentThread().getName());
+                System.out.println("Current thread priority is  "+Thread.currentThread().getPriority());
+            }
+        });
+        thread.setName("Revolt-GPG");
+        thread.setPriority(1);
+        System.out.println("We are now in thread "+Thread.currentThread().getName()+" before starting a new thread");
+        thread.start();
+        System.out.println("We are now in thread "+Thread.currentThread().getName()+" after starting a new thread");
+    }
+}
