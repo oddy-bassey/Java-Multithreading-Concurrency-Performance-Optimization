d70a2c198326f6cd9898d75f83c5d0d044f6a673
oddy-bassey <oddybassey@gmail.com>
2022-12-10 07:16:27 +0100

    handling uncaught exceptions in a thread

--- mutithreading/src/Main.java
+++ mutithreading/src/Main.java
@@ -4,15 +4,16 @@ public class Main {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-
-                System.out.println("We are now in thread "+Thread.currentThread().getName());
-                System.out.println("Current thread priority is  "+Thread.currentThread().getPriority());
+                throw new RuntimeException("I will break down this thread completely!");
             }
         });
         thread.setName("Revolt-GPG");
-        thread.setPriority(1);
-        System.out.println("We are now in thread "+Thread.currentThread().getName()+" before starting a new thread");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread "+t.getName()+". The error is: "+e.getLocalizedMessage());
+            }
+        });
         thread.start();
-        System.out.println("We are now in thread "+Thread.currentThread().getName()+" after starting a new thread");
     }
 }
