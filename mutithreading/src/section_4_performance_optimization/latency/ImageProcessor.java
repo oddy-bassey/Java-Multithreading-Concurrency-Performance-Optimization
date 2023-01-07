d2ddcfd839e6630c7c3fa784a06a749e5a7c5356
oddy-bassey <oddybassey@gmail.com>
2023-01-07 21:44:06 +0100

    throughput impl

--- mutithreading/src/section_4_performance_optimization/latency/ImageProcessor.java
+++ mutithreading/src/section_4_performance_optimization/latency/ImageProcessor.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    private static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+public class ImageProcessor {
+    private static final String SOURCE_FILE = "./resources/latency/many-flowers.jpg";
     private static final String DESTINATION_FILE = "./out/many-flowers.jpg";
     public static void main(String[] args) throws IOException {
         BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
---
+++ mutithreading/src/section_4_performance_optimization/throughput/ThroughputHttpServer.java
@@ -0,0 +1,70 @@
+package section_4_performance_optimization.throughput;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ThroughputHttpServer {
+    private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+    private static final int NUMBER_OF_THREADS = 1;
+    public static void main(String[] args) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+        startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        httpServer.createContext("/search", new WordHandler(text));
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        httpServer.setExecutor(executor);
+        httpServer.start();
+    }
+
+    private static class WordHandler implements HttpHandler {
+        private String text;
+        public WordHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+            if (!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            long count = countWord(word);
+            byte[] response = Long.toString(count).getBytes();
+            httpExchange.sendResponseHeaders(200, response.length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
+        }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+            while (index >= 0){
+                index = text.indexOf(word, index);
+
+                if (index >= 0) {
+                    count++;
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
+}
