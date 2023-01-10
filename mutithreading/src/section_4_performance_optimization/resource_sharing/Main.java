e3b2808985ae913338ccc13b3ae91cc7a5195771
oddy-bassey <oddybassey@gmail.com>
2023-01-10 13:21:38 +0100

    updating a shared resource impl with unjoined threads

---
+++ mutithreading/src/section_4_performance_optimization/resource_sharing/Main.java
@@ -0,0 +1,61 @@
+package section_4_performance_optimization.resource_sharing;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("Items available : "+inventoryCounter.getItems());
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
