6ac6c30c2883025985eca7c624bbb0eb68bdc22e
oddy-bassey <oddybassey@gmail.com>
2023-01-10 15:41:31 +0100

    resolving concurrent resource access by declaring a method with the synchronized keyword (Monitor)

---
+++ mutithreading/src/section_5_concurrency_challenges_and_solutions/critical_section_and_synchronization/Main.java
@@ -0,0 +1,61 @@
+package section_5_concurrency_challenges_and_solutions.critical_section_and_synchronization;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("Items available : "+inventoryCounter.getItems());
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public synchronized void increment() {
+            items++;
+        }
+
+        public synchronized void decrement() {
+            items--;
+        }
+
+        public synchronized int getItems() {
+            return items;
+        }
+    }
+}
