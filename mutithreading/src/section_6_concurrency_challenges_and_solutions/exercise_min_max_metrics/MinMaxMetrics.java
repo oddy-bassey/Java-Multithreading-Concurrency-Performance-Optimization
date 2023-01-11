83d6ff5b905677f6622584e0d954490619aca2e9
oddy-bassey <oddybassey@gmail.com>
2023-01-12 00:54:28 +0100

    my MinMaxMetrics solution

---
+++ mutithreading/src/section_6_concurrency_challenges_and_solutions/exercise_min_max_metrics/MinMaxMetrics.java
@@ -0,0 +1,45 @@
+package section_6_concurrency_challenges_and_solutions.exercise_min_max_metrics;
+
+public class MinMaxMetrics {
+
+    // Add all necessary member variables
+    private volatile long minimumPrice;
+    private volatile long maximumPrice;
+
+    /**
+     * Initializes all member variables
+     */
+    public MinMaxMetrics() {
+        // Add code here
+        this.minimumPrice = Long.MAX_VALUE;
+        this.maximumPrice = Long.MIN_VALUE;
+    }
+
+    /**
+     * Adds a new sample to our metrics.
+     */
+    public synchronized void addSample(long newSample) {
+        // Add code here
+        if (newSample < minimumPrice) {
+            minimumPrice = newSample;
+        } else if (newSample > maximumPrice) {
+            maximumPrice = newSample;
+        }
+    }
+
+    /**
+     * Returns the smallest sample we've seen so far.
+     */
+    public long getMin() {
+        // Add code here
+        return minimumPrice;
+    }
+
+    /**
+     * Returns the biggest sample we've seen so far.
+     */
+    public long getMax() {
+        // Add code here
+        return maximumPrice;
+    }
+}
