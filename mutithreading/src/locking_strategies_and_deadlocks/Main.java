cf36a8222fb9d4897750915342fe54df943f1181
oddy-bassey <oddybassey@gmail.com>
2023-01-15 16:34:23 +0100

    resolving the deadlock by implementing a strict locking order

--- mutithreading/src/locking_strategies_and_deadlocks/Main.java
+++ mutithreading/src/locking_strategies_and_deadlocks/Main.java
@@ -73,10 +73,10 @@ public class Main {
         }
 
         public void takeRoadB () {
-            synchronized (roadB) {
-                System.out.println("Road B is locked by "+Thread.currentThread().getName());
+            synchronized (roadA) {
+                System.out.println("Road A is locked by "+Thread.currentThread().getName());
 
-                synchronized (roadA) {
+                synchronized (roadB) {
                     System.out.println("Train is passing through road B");
                     try {
                         Thread.sleep(1);
